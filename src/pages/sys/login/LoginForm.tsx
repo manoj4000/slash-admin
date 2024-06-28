e4ccc53d0d7ee26d58ac06ebfc241f3748f0dc05
manoj4000 <74961587+manoj4000@users.noreply.github.com>
2024-06-28 10:22:38 +0530

    Update LoginForm.tsx

--- src/pages/sys/login/LoginForm.tsx
+++ src/pages/sys/login/LoginForm.tsx
@@ -1,18 +1,19 @@
-import {  Button, Checkbox, Col, Divider, Form, Input, Row } from 'antd';
+import { Alert, Button, Checkbox, Col, Divider, Form, Input, Row } from 'antd';
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { AiFillGithub, AiFillGoogleCircle, AiFillWechat } from 'react-icons/ai';
 
-import { DEFAULT_USER } from '@/_mock/assets';
+import { DEFAULT_USER, TEST_USER } from '@/_mock/assets';
 import { SignInReq } from '@/api/services/userService';
 import { useSignIn } from '@/store/userStore';
-
+import ProTag from '@/theme/antd/components/tag';
+import { useThemeToken } from '@/theme/hooks';
 
 import { LoginStateEnum, useLoginStateContext } from './providers/LoginStateProvider';
 
 function LoginForm() {
   const { t } = useTranslation();
-  
+  const themeToken = useThemeToken();
   const [loading, setLoading] = useState(false);
 
   const { loginState, setLoginState } = useLoginStateContext();
@@ -28,32 +29,57 @@ function LoginForm() {
       setLoading(false);
     }
   };
+
   return (
     <>
       <div className="mb-4 text-2xl font-bold xl:text-3xl">{t('sys.login.signInFormTitle')}</div>
       <Form
         name="login"
         size="large"
-        initialValues={{
-          remember: false,
-          username: DEFAULT_USER.username,
-          password: DEFAULT_USER.password,
-        }}
         onFinish={handleFinish}
       >
-       
+        <div className="mb-4 flex flex-col">
+          <Alert
+            type="info"
+            description={
+              <div className="flex flex-col">
+                <div className="flex">
+                  <ProTag className="flex-shrink-0">Admin {t('sys.login.userName')}:</ProTag>
+                  <strong className="ml-1" style={{ color: themeToken.colorInfoTextHover }}>
+                    <span>{DEFAULT_USER.username}</span>
+                  </strong>
+                </div>
+
+                <div className="flex">
+                  <ProTag className="flex-shrink-0">Test {t('sys.login.userName')}:</ProTag>
+                  <strong className="ml-1" style={{ color: themeToken.colorInfoTextHover }}>
+                    <span>{TEST_USER.username}</span>
+                  </strong>
+                </div>
+
+                <div>
+                  <ProTag className="flex-shrink-0">{t('sys.login.password')}:</ProTag>
+                  <strong className=" ml-1" style={{ color: themeToken.colorInfoTextHover }}>
+                    {DEFAULT_USER.password}
+                  </strong>
+                </div>
+              </div>
+            }
+            showIcon
+          />
+        </div>
 
         <Form.Item
           name="username"
-          rules={[{ required: true,  }]}
+          rules={[{ required: true, message: t('sys.login.accountPlaceholder') }]}
         >
-          <Input placeholder="username" />
+          <Input placeholder={t('sys.login.userName')} />
         </Form.Item>
         <Form.Item
           name="password"
-          rules={[{ required: true,  }]}
+          rules={[{ required: true, message: t('sys.login.passwordPlaceholder') }]}
         >
-          <Input.Password type="password" placeholder="password" />
+          <Input.Password type="password" placeholder={t('sys.login.password')} />
         </Form.Item>
         <Form.Item>
           <Row>
